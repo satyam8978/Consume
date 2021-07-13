30b166415c8b52e0045ef07b28e0f49ac5d47c89
satyam jatla <satyam0297@gmail.com>
2021-07-13 10:25:42 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Consume.Models;
+
+namespace Consume.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        string Baseurl = "https://localhost:5001/";
+        public async Task<IActionResult> IndexAsync()
+        {
+            List<Employee> EmpInfo = new List<Employee>();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+                HttpResponseMessage Res = await client.GetAsync("api/EmployeeDetails/");
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+                    //Deserializing the response recieved from web api and storing into the Employee list
+                    EmpInfo = JsonConvert.DeserializeObject<List<Employee>>(EmpResponse);
+                }
+                //returning the employee list to view
+                return View(EmpInfo);
+            }
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Consume.Models
+{
+    public class Employee
+    {
+       
+          
+           
+            public string EmpId { get; set; }
+            public string EMPName { get; set; }
+            public string EmailId { get; set; }
+            public string MobileNo { get; set; }
+            public List<string> ProjectID { get; set; }
+
+            public List<string> TeamId { get; set; }
+
+
+
+        
+    }
+}
